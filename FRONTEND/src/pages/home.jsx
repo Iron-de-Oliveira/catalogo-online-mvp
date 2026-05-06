1d4c9a0c32632458939d7b99405a4c2844cafbf9
sousavz <vitoriadossantossousah@gmail.com>
2026-05-06 15:31:03 -0300

    feat: atualizações na home e estrutura de páginas

---
+++ FRONTEND/src/pages/home.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import "../home.css";
+
+
+
+function Home() {
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <h1>Welcome to the Home Page</h1>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Home;
