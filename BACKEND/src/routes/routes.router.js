cf5ddfc7f796510deaf26f715fff6eebfc8d1fba
Iron-de-Oliveira <irondeoliveira3@gmail.com>
2026-05-20 22:05:34 -0300

    erro conflitos 2

--- BACKEND/src/routes/routes.router.js
+++ BACKEND/src/routes/routes.router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const auth = require('../middlewares/auth')
 const admin = require('../middlewares/admin')
+const upload = require('../middlewares/upload')
 
 const produtoController = require('../controllers/ProdutoController')
 const administradorController = require('../controllers/AdministradorController')
@@ -35,35 +36,21 @@ routes.use('/auth', authRoutes)
 routes.post('/admin/login', administradorController.login)
 routes.post('/administradores', administradorController.criar)
 
-<<<<<<< HEAD
 // Produtos públicos
 routes.get('/produtos', produtoController.listar)
 routes.get('/produtos/id/:id', produtoController.listarPorId)
 routes.get('/produtos/categoria/:categoria', produtoController.listarPorCategoria)
-=======
-// Rotas para produtos
-routes.get('/produtos', produtoController.listar)
-routes.get('/produtos/id/:id', produtoController.listarPorId)
-routes.get('/produtos/categoria/:categoria', auth, produtoController.listarPorCategoria)
-routes.put('/produtos/id/:id', auth, produtoController.atualizar)
-routes.post('/produtos', auth, produtoController.criar)
-routes.delete('/produtos/id/:id', auth, produtoController.deletar)
->>>>>>> 6c358cda88dfd19c2ae80b99de77739ef3bb85f1
 
 // Produtos protegidos
-routes.post('/produtos', auth, admin,produtoController.criar)
+routes.post('/produtos', auth, admin, produtoController.criar)
 routes.put('/produtos/id/:id', auth, admin, produtoController.atualizar)
 routes.delete('/produtos/id/:id', auth, admin, produtoController.deletar)
 
 // Usuários
 routes.post('/usuarios', userController.criar)
-<<<<<<< HEAD
 
 // Usuários protegidos
 routes.get('/usuarios', auth, userController.listar)
-=======
-routes.get('/usuarios', userController.listar)
->>>>>>> 6c358cda88dfd19c2ae80b99de77739ef3bb85f1
 routes.get('/usuarios/id/:id', auth, userController.encontrarPorId)
 routes.get('/usuarios/email/:email', auth, userController.encontrarPorEmail)
 routes.delete('/usuarios/email/:email', auth, userController.deletarPorEmail)
