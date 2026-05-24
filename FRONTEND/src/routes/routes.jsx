b69462e027ffc959f4cfe2b21613d98b754c1e0e
sousavz <vitoriadossantossousah@gmail.com>
2026-05-24 16:07:36 -0300

    feat: adiciona tela de perfil do usuario

--- FRONTEND/src/routes/routes.jsx
+++ FRONTEND/src/routes/routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import LoginPage from "../pages/login";
 import ExibProduto from "../pages/exibProduto";
+import PerfilUsuario from "../pages/perfilusuario";
 
 function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/produto/:id" element={<ExibProduto />} />
+        <Route path="/perfil" element={<PerfilUsuario />} />
       </Routes>
     </BrowserRouter>
   );
